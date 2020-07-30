52f47b35d245a13ecae20d8c8b165444e88e269f
Danila Samuseu <Danila_Samuseu@epam.com>
2020-07-30 21:39:13 +0300

    Added base logic for Module 5

---
+++ Module5/FolderVisor/FolderVisor.ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace FolderVisor.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> patternToFolder = new Dictionary<string, string>()
+            {
+                {".txt", "C:\\Users\\Danila_Samuseu\\Desktop\\HomeWork\\Module5\\Txt"},
+                {".docx", "C:\\Users\\Danila_Samuseu\\Desktop\\HomeWork\\Module5\\Docx"},
+            };
+
+            SystemWatcher watcher = new SystemWatcher(patternToFolder);
+            watcher.Run();
+        }
+    }
+}
---
+++ Module5/FolderVisor/FolderVisor.ConsoleUI/SystemWatcher.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FolderVisor.ConsoleUI
+{
+    public class SystemWatcher
+    {
+        public Dictionary<string, string> PatternToFolder { get; set; }
+        public string DefaultFilePath { get; set; }
+        public bool ShouldAddId { get; set; }
+        public bool ShouldAddCreationDate { get; set; }
+        public int CurrentId { get; set; }
+
+        public SystemWatcher(Dictionary<string, string> patternToFolder)
+        {
+            PatternToFolder = patternToFolder;
+            DefaultFilePath = "C:\\Users\\Danila_Samuseu\\Desktop\\HomeWork\\Module5\\Default";
+            ShouldAddId = true;
+            ShouldAddCreationDate = true;
+            CurrentId = 1;
+        }
+
+        public void Run()
+        {
+            string[] folderPaths = { "C:\\Users\\Danila_Samuseu\\Desktop\\HomeWork\\Module5", "C:\\Users\\Danila_Samuseu\\Desktop\\HomeWork\\Module4" };
+
+            foreach (var folderPath in folderPaths)
+            {
+                Watch(folderPath);
+            }
+
+            while (true) ;
+        }
+
+        public void Watch(string path)
+        {
+            var watcher = new FileSystemWatcher();
+
+            watcher.Path = path;
+            watcher.NotifyFilter = NotifyFilters.LastWrite
+                                   | NotifyFilters.FileName;
+            watcher.Filter = "";
+            watcher.Created += HandleFileCreated;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void HandleFileCreated(object source, FileSystemEventArgs e)
+        {
+            Log($"New file {e.FullPath} detected. Creation date: {DateTime.Now}");
+            MoveFile(e.FullPath);
+        }
+
+        private void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private void MoveFile(string filePath)
+        {
+            string fileName = GetFileNameByFullPath(filePath);
+
+            string creationDate = ShouldAddCreationDate ? $"{DateTime.Now:dd.MM.yyyy}" : "";
+            string id = ShouldAddId ? $"№{CurrentId}" : "";
+
+            foreach (var keyValue in PatternToFolder)
+            {
+                if (filePath.Contains(keyValue.Key))
+                {
+                    Log($"{filePath} mathes the patter '{keyValue.Key}'!");
+                    string destination = $"{keyValue.Value}\\{creationDate}{id}{fileName}";
+                    File.Move(filePath, destination);
+                    Log($"{filePath} was moved to {destination}");
+                    return;
+                }
+            }
+
+            Log($"{filePath} not matches any patterns!");
+            File.Move(filePath, $"{DefaultFilePath}\\{fileName}");
+            Log($"{filePath} was moved to default folder {DefaultFilePath}");
+            CurrentId++;
+        }
+
+        private string GetFileNameByFullPath(string path)
+        {
+            string[] splitPath = path.Split("\\");
+
+            return splitPath.Last();
+        }
+    }
+}
